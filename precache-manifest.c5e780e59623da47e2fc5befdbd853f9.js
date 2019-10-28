35bbe6c38aaf5654f2610f36057ac687ebcadd55
CN72 <cn72@CN72.local>
2019-10-28 15:34:27 +0000

    Updates

---
+++ precache-manifest.c5e780e59623da47e2fc5befdbd853f9.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334b3292c58d58af176429635a1c617f",
+    "url": "/mw-portfolio/index.html"
+  },
+  {
+    "revision": "4726e31333f415d6e434",
+    "url": "/mw-portfolio/static/css/main.ea226a5f.chunk.css"
+  },
+  {
+    "revision": "ab393566d0376c58ade2",
+    "url": "/mw-portfolio/static/js/2.7e7d5e84.chunk.js"
+  },
+  {
+    "revision": "4726e31333f415d6e434",
+    "url": "/mw-portfolio/static/js/main.b235c6e0.chunk.js"
+  },
+  {
+    "revision": "dc58b544765ebc47f8c5",
+    "url": "/mw-portfolio/static/js/runtime-main.bc063c39.js"
+  },
+  {
+    "revision": "461957f1654b93513b6ec7eba7a1e337",
+    "url": "/mw-portfolio/static/media/Developer-CV.461957f1.pdf"
+  },
+  {
+    "revision": "11b3e6f43698416774bc496845ec6a1e",
+    "url": "/mw-portfolio/static/media/address-book-react.11b3e6f4.png"
+  },
+  {
+    "revision": "96d69d4ec1ceee042a79b60980e5d737",
+    "url": "/mw-portfolio/static/media/black-dice-game.96d69d4e.png"
+  },
+  {
+    "revision": "e5635ab90c4b1b25d7738dbeeda693c3",
+    "url": "/mw-portfolio/static/media/brand3.e5635ab9.png"
+  },
+  {
+    "revision": "4458bf26a3c66683408626b791a2c99d",
+    "url": "/mw-portfolio/static/media/cyber-pet-game.4458bf26.png"
+  },
+  {
+    "revision": "0e2c253b6d7662b08b972f3a9b432783",
+    "url": "/mw-portfolio/static/media/drumkit-cat.0e2c253b.png"
+  },
+  {
+    "revision": "e9fb3fe2ba8c901ea43106621f68e16c",
+    "url": "/mw-portfolio/static/media/express-news.e9fb3fe2.png"
+  },
+  {
+    "revision": "616a3d32be9611a71560f68ce09cc0d8",
+    "url": "/mw-portfolio/static/media/first-rpg-game.616a3d32.png"
+  },
+  {
+    "revision": "b6ec19ceacb23390aa66d2f101d0ddf6",
+    "url": "/mw-portfolio/static/media/github.b6ec19ce.png"
+  },
+  {
+    "revision": "f0ebc33ab0aa128225f3a31f10ce73e0",
+    "url": "/mw-portfolio/static/media/keycode-generator.f0ebc33a.png"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/mw-portfolio/static/media/linkedin.fd0d5546.png"
+  },
+  {
+    "revision": "b42824debd128038325a23fe907f3b5b",
+    "url": "/mw-portfolio/static/media/london-bus-animation.b42824de.png"
+  },
+  {
+    "revision": "80fb2b181eab4688268ef62f77aecd25",
+    "url": "/mw-portfolio/static/media/netflix-copy.80fb2b18.png"
+  },
+  {
+    "revision": "63a4e04d7d62d2a59006fd0774b5fc80",
+    "url": "/mw-portfolio/static/media/profile-pic.63a4e04d.jpg"
+  },
+  {
+    "revision": "78bb4717163d77fff63f87aad0555931",
+    "url": "/mw-portfolio/static/media/profile.78bb4717.png"
+  },
+  {
+    "revision": "4d6897d74f8474504f5c96b3c32b3214",
+    "url": "/mw-portfolio/static/media/simple-calculator-react.4d6897d7.png"
+  },
+  {
+    "revision": "01883d9f78ca80cb335264faebe40fdf",
+    "url": "/mw-portfolio/static/media/simplyRota.01883d9f.png"
+  },
+  {
+    "revision": "20cd9331c6b8b8c66cac851d87b7d1ab",
+    "url": "/mw-portfolio/static/media/swift-dice-game-app.20cd9331.png"
+  },
+  {
+    "revision": "2ad914910aa4213d6d57186386df47be",
+    "url": "/mw-portfolio/static/media/weather-retriever.2ad91491.png"
+  }
+]);
